cb2982f4ff71d23eb0da02cecf75bc21640b5a13
Treinamento <treinamento@treina1.senado.gov.br>
2017-10-26 13:02:15 -0200

    Finish pug template

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var Extractor = require('extract-text-webpack-plugin')
 module.exports = {
   entry: {
     'main': './app/scripts/main.js',
-    'styles': './app/styles/main.less',
+    'styles': './app/styles/styles.less',
   },
   output: {
     path: path.resolve('./dist'),
@@ -27,22 +27,10 @@ module.exports = {
   },
   plugins: [
     new Extractor('[name].css')
-  ],  module: {
-    rules: [{
-      test: /\.pug$/, loader: 'pug-loader'
-    }, {
-      test: /\.less$/,
-      loader: Extractor.extract(['css-loader', 'less-loader'])
-    }, {
-      test: /\.(svg|woff|ttf|eot|woff2)$/,
-      loaders: ['file-loader?name=fonts/[name].[hash:base64:5].[ext]']
-    }]
-  },
-  plugins: [
-    new Extractor('[name].css')
   ],
   devServer: {
     contentBase: path.resolve('./app'),
     publicPath: '/dist/'
   }
 };
+
